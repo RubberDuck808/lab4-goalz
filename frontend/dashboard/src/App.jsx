bd7ddbca58a9828ed327f81b1d6811e6ea723106
Giel van Gorp <gielvgorp123@gmail.com>
2026-04-10 16:25:44 +0200

    Created ui for login dashboard
     - Recreated react project because fucked it up
     - Added login.jsx: for ui
     - Added authservice: to call endpoint for authentication
     - Added stylings: added colors in app.css for dashboard

---
+++ frontend/dashboard/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
