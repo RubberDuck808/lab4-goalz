1502a884dfaa2006611a1024c416cabfc817ca1f
Lukas Povilaitis <l.povilaitis@student.fontys.nl>
2026-04-15 14:11:14 +0200

    Added Map Elements for the dashboard so you can retrieve sensor and element data. Created Get Endpoint for it.

---
+++ backend/Goalz/Goalz.Application/Interfaces/IOverviewService.cs
@@ -0,0 +1,10 @@
+﻿using Goalz.Core.DTOs;
+using Goalz.Domain.Entities;
+
+namespace Goalz.Core.Interfaces;
+
+public interface IOverviewService
+{
+    Task<ElementsDTO> GetDashboardData();
+    
+}
---
+++ backend/Goalz/Goalz.Domain/Entities/Information.cs
@@ -0,0 +1,10 @@
+﻿namespace Goalz.Domain.Entities;
+
+public class Information
+{
+    public long Id { get; set; }
+    public long InfoTxt { get; set; }
+    public long NewColumn { get; set; }
+
+    public ICollection<Sensor> Sensors { get; set; } = new List<Sensor>();
+}
